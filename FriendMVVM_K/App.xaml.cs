ea5052021fcf33a6c81bc677cc44280acd2fbae9
opilane <opilane@HP-CZC6297B9H>
2021-01-27 10:26:58 +0200

    Add project files.

---
+++ FriendMVVM_K/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using FriendMVVM_K.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FriendMVVM_K
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new FriendsListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
